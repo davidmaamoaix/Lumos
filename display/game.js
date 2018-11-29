a4c9ce8eb401e69f68729139cd945c43cc313535
David Ma <davidma@davidma.cn>
2018-11-29 20:41:24 +0800

    Yo multiple shadows ya boi

--- display/game.js
+++ display/game.js
@@ -1,5 +1,6 @@
 var ctx;
 var mouse;
+var shapes;
 var doesUpdate;
 var canvas=document.getElementById('game');;
 
@@ -23,7 +24,7 @@ function update(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 
 	// Setup
-	var shapes=[ // [x1 y1 x2 y2]
+	shapes=[ // [x1 y1 x2 y2]
 				// Border
 		[[0,0],[1080,0],[1080,1],[0,1]],
 		[[0,0],[0,720],[1,720],[1,0]],
@@ -40,68 +41,26 @@ function update(){
 		[[930,135],[960,135],[980,155],[930,155],[930,200],[960,200],[980,220],[980,265],[960,285],
 		[930,285],[910,265],[960,265],[960,220],[930,220],[910,200],[910,155]]]; // S (910-980)
 
-	// Def Ray
-	var mid=[canvas.width/2,canvas.height/2];
+	
+	var vertices=get_vertices(mouse);
+	draw_polygon(vertices,"#FFF");
 
-	// Vertices
-	var vertices=[];
-	shapes.forEach(function(shape){
-		shape.forEach(function(vertex){
-			var duplicate=function(array,vertex){
-				for(var i=0;i<vertices.length;i++){
-					if(vertices[i][0]==vertex[0]&&vertices[i][1]==vertex[1]) return true;
-				}
-				return false;
-			};
-			if(!duplicate(vertices,vertex)){
-
-				// Get Radians
-				var curr_angle=get_radians(mouse,vertex);
-				vertices.push(vertex.concat([curr_angle]));
-
-				// Get Offset
-				var get_vertex=function(radians,vertex){
-					var dx=10;
-					return [vertex[0]+dx,vertex[1]+Math.tan(radians)*dx]
-				};
-
-				var offset=[curr_angle-0.001,curr_angle+0.001];
-				vertices.push([mouse[0]+Math.cos(offset[0]),mouse[1]+Math.sin(offset[0]),offset[0]]);
-				vertices.push([mouse[0]+Math.cos(offset[1]),mouse[1]+Math.sin(offset[1]),offset[1]]);
-			}
-		});
-	});
-
-	//Sort Counter-Clockwise
-	vertices=vertices.sort(function(a,b){return a[2]-b[2]});
-
-	var polygon=[];
-	vertices.forEach(function(vertex){
-
-		// Closest
-		var min_cross=null;
-		for(var i=0;i<shapes.length;i++){
-			var sides=shapes[i].length;
-			for(var j=0;j<sides;j++){
-				var temp_cross=get_intersect([mouse,vertex],[shapes[i][j%sides],shapes[i][(j+1)%sides]]);
-				if(temp_cross==null) continue;
-				if(min_cross==null||distance(mouse,temp_cross)<distance(mouse,min_cross)){
-					min_cross=temp_cross;
-				}
-			}
-		}
-
-		if(min_cross!=null) polygon.push(min_cross);
-	});
-
-	//Draw Polygon
-	ctx.beginPath();
-	ctx.moveTo(polygon[0][0],polygon[0][1]);
-	for(var i=1;i<polygon.length;i++){
-		ctx.lineTo(polygon[i][0],polygon[i][1]);
+	// Offset the mouse to create fuzzy shadows
+	var offset_len=12.5;
+	var offset_count=10;
+	var gradient_alpha=1;
+	for(var i=0;i<2*Math.PI;i+=2*Math.PI/offset_count){
+		var vertices=get_vertices([mouse[0]+(offset_len*Math.cos(i)),mouse[1]+(offset_len*Math.sin(i))]);
+		draw_polygon(vertices,"rgba(255,255,255,"+(gradient_alpha/offset_count)+")");
 	}
-	ctx.fillStyle="#FFF";
-	ctx.fill();
+
+	/*ctx.beginPath();
+	polygon.forEach(function(vertex){
+		ctx.moveTo(mouse[0],mouse[1]);
+		ctx.lineTo(vertex[0],vertex[1]);
+		ctx.strokeStyle="#F00";
+		ctx.stroke();
+	});*/
 
 	// Draw Shapes
 	draw_shapes(shapes);
@@ -123,6 +82,73 @@ function draw_shapes(shapes){
 	}
 	ctx.strokeStyle="#FFF";
 	ctx.stroke();
+}
+
+function draw_polygon(points,style){
+	//Draw Polygon
+	ctx.beginPath();
+	ctx.moveTo(points[0][0],points[0][1]);
+	for(var i=1;i<points.length;i++){
+		ctx.lineTo(points[i][0],points[i][1]);
+	}
+	ctx.fillStyle=style;
+	ctx.fill();
+}
+
+function get_vertices(mouse_pos){
+	// Vertices
+	// Vertices
+	var vertices=[];
+	shapes.forEach(function(shape){
+		shape.forEach(function(vertex){
+			var duplicate=function(array,vertex){
+				for(var i=0;i<vertices.length;i++){
+					if(vertices[i][0]==vertex[0]&&vertices[i][1]==vertex[1]) return true;
+				}
+				return false;
+			};
+			if(!duplicate(vertices,vertex)){
+
+				// Get Radians
+				var curr_angle=get_radians(mouse_pos,vertex);
+				vertices.push(vertex.concat([curr_angle]));
+
+				// Get Offset
+				var get_vertex=function(radians,vertex){
+					var dx=10;
+					return [vertex[0]+dx,vertex[1]+Math.tan(radians)*dx]
+				};
+
+				var offset=[curr_angle-0.001,curr_angle+0.001];
+				vertices.push([mouse_pos[0]+Math.cos(offset[0]),mouse_pos[1]+Math.sin(offset[0]),offset[0]]);
+				vertices.push([mouse_pos[0]+Math.cos(offset[1]),mouse_pos[1]+Math.sin(offset[1]),offset[1]]);
+			}
+		});
+	});
+
+	//Sort Counter-Clockwise
+	vertices=vertices.sort(function(a,b){return a[2]-b[2]});
+
+	var polygon=[];
+	vertices.forEach(function(vertex){
+
+		// Closest
+		var min_cross=null;
+		for(var i=0;i<shapes.length;i++){
+			var sides=shapes[i].length;
+			for(var j=0;j<sides;j++){
+				var temp_cross=get_intersect([mouse_pos,vertex],[shapes[i][j%sides],shapes[i][(j+1)%sides]]);
+				if(temp_cross==null) continue;
+				if(min_cross==null||distance(mouse_pos,temp_cross)<distance(mouse_pos,min_cross)){
+					min_cross=temp_cross;
+				}
+			}
+		}
+
+		if(min_cross!=null) polygon.push(min_cross);
+	});
+
+	return polygon;
 }
 
 canvas.onmousemove=function(e){
