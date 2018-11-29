1e710d9963c6db9acf0439cb9bbfc052ed4a3422
David Ma <davidma@davidma.cn>
2018-11-29 09:49:48 +0800

    Temp Changes

--- display/game.js
+++ display/game.js
@@ -1,9 +1,11 @@
 var ctx;
 var mouse;
+var doesUpdate;
 var canvas=document.getElementById('game');;
 
 function start(){
 	mouse=[0,0]
+	doesUpdate=true;
 	ctx=canvas.getContext('2d');
 	canvas.width=1080;
 	canvas.height=720;
@@ -14,30 +16,87 @@ function start(){
 
 function update(){
 
+	if(doesUpdate) doesUpdate=false;
+	else return;
+
 	// Init
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 
-	// Render Shapes
-	var shapes=[[[100,100],[200,150],[100,250]],
+	// Setup
+	var shapes=[
+				// Border
+				[[0,0],[1080,0],[1080,1],[0,1]],
+				[[0,0],[0,720],[1,720],[1,0]],
+				[[0,720],[1080,720],[1080,719],[0,719]],
+				[[1080,0],[1080,720],[1079,720],[1079,0]],
+
+				// Level				
+				[[100,100],[200,150],[100,250]],
 				[[200,200],[350,450],[275,500],[150,350]],
 				[[700,300],[900,300],[900,400],[700,400]]]; // [x1 y1 x2 y2]
-	draw_shapes(shapes);
 
 	// Def Ray
 	var mid=[canvas.width/2,canvas.height/2];
 
-	//Closest
-	var min_cross=null;
-	for(var i=0;i<shapes.length;i++){
-		var sides=shapes[i].length;
-		for(var j=0;j<sides;j++){
-			var temp_cross=get_intersect(mid,mouse,shapes[i][j%sides],shapes[i][(j+1)%sides]);
-			if(temp_cross!=null){
+	// Vertices
+	var vertices=[];
+	shapes.forEach(function(shape){
+		shape.forEach(function(vertex){
+			var duplicate=function(array,vertex){
+				for(var i=0;i<vertices.length;i++){
+					if(vertices[i][0]==vertex[0]&&vertices[i][1]==vertex[1]) return true;
+				}
+				return false;
+			};
+			if(!duplicate(vertices,vertex)){
+				vertices.push(vertex);
 
+				// Get Radians
+				var dx=vertex[0]-mouse[0];
+				var dy=vertex[1]-mouse[1];
+				var curr_angle=Math.atan(Math.abs(dy/dx));
+				if(dx*dy<0) curr_angle=Math.PI/2-curr_angle; // Quadrant I & III
+				var multiplier=0;
+				if(dx<0) multiplier++; // Quadrant II & III
+				if(dy<0) multiplier++; // Quadrant III & IV
+				if(dx>0&&dy<0) multiplier+=2; // Quadrant IV
+				curr_angle+=Math.PI/2*multiplier;
+
+				// Get Offset
+				var get_vertex=function(radian,vertex){
+					var dx=vertex[0]+10;
+					return [dx,Math.tan(radian)*dx]
+				};
+				//vertices.push(get_vertex(curr_angle,vertex));
+				//vertices.push(get_vertex(curr_angle-0.00001,vertex));
 			}
-		}
-	}
+		});
+	});
 
+	vertices.forEach(function(vertex){
+
+		// Closest
+			var min_cross=null;
+			for(var i=0;i<shapes.length;i++){
+				var sides=shapes[i].length;
+				for(var j=0;j<sides;j++){
+					var temp_cross=get_intersect([mouse,vertex],[shapes[i][j%sides],shapes[i][(j+1)%sides]]);
+					if(temp_cross==null) continue;
+					if(min_cross==null||distance(mouse,temp_cross)<distance(mouse,min_cross)){
+						min_cross=temp_cross;
+					}
+				}
+			}
+			// Draw Closest
+			ctx.beginPath()
+			ctx.moveTo(mouse[0],mouse[1]);
+			ctx.lineTo(min_cross[0],min_cross[1]);
+			ctx.strokeStyle="#00F";
+			ctx.stroke();
+	});
+
+	// Draw Shapes
+	draw_shapes(shapes);
 }
 
 function draw_shapes(shapes){
@@ -53,7 +112,9 @@ function draw_shapes(shapes){
 }
 
 canvas.onmousemove=function(e){
-	mouse=[e.clientX,e.clientY];
+	var rect=canvas.getBoundingClientRect();
+	mouse=[e.clientX-rect.left,e.clientY-rect.top];
+	doesUpdate=true;
 };
 
 document.onload=start();
--- math/calculations.js
+++ math/calculations.js
@@ -1,5 +1,20 @@
 function get_intersect(ray,line){
-	
+
+	// Changes in Position
+	var ray_dx=ray[1][0]-ray[0][0];
+	var ray_dy=ray[1][1]-ray[0][1];
+	var line_dx=line[1][0]-line[0][0];
+	var line_dy=line[1][1]-line[0][1];
+
+	// Parallel
+	if(line_dy/line_dx==ray_dy/ray_dx) return null;
+
+	var delta=((line[0][1]-ray[0][1])*ray_dx+(ray[0][0]-line[0][0])*ray_dy)/(ray_dy*line_dx-ray_dx*line_dy);
+	var percentage=(line[0][0]+line_dx*delta-ray[0][0])/ray_dx;
+
+	if(percentage<0||delta<0||delta>1) return null;
+
+	return [ray[0][0]+ray_dx*percentage,ray[0][1]+ray_dy*percentage];
 }
 
 function distance(a,b){
